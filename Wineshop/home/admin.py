a9e3d0b07dd0b0563c5ecc6de26192f90b4650b0
Blank619 <vipulshide0597@gmail.com>
2020-07-02 23:28:49 +0530

    add products created.

---
+++ Wineshop/home/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from django import forms
+# Register your models here.
+from .models import RegisterItem
+
+admin.site.register(RegisterItem)
---
+++ Wineshop/home/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django import forms
+
+
+class RegisterItem(models.Model):
+    name = models.CharField(max_length=120)
+    Category = models.CharField(max_length=500)
+    quantity = models.IntegerField(default=0)
+    Price = models.IntegerField()
+
+
+    def __str__(self):
+        return  self.name
